b6644f95d8692bba19ac39316f71c9c1bb9f3e3e
UncleSamChowder <745603@student.dusd.net>
2025-03-20 10:50:23 -0700

    bfb
    fbdb

--- Unit1SamuelCeperoDraft/Assets/Challenge 1/Scripts/FollowPlayer.cs
+++ Unit1SamuelCeperoDraft/Assets/Challenge 1/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 6, -7);
+    private Vector3 offset = new Vector3(0, 6.96f, -9.7f);
     // Start is called before the first frame update
     void Start()
     {
